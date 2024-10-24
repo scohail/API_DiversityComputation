5fd5bdc88297f680fe7359c86bee0a9544931d85
scohail <scohail206@gmail.com>
2024-10-24 01:01:26 +0100

    Update Classification_Diversity.py

--- Classification_Diversity.py
+++ Classification_Diversity.py
@@ -1,3 +1,35 @@
+import numpy as np
+
+
+
+
+def len_check(Y_model1, Y_model, Y_obs):
+    try:
+        len(Y_model1) == len(Y_model) == len(Y_obs)
+    except ValueError:
+        print('Error: The length of the arrays are not equal')
+        return False
+    return True
+
+def Binary_Matix(Y_1 ,Y_2 ,Y_obs):
+    BM = np.zeros((2,2))
+    try:
+        len(Y_1) == len(Y_2) and len(Y_2)  == len(Y_obs)
+
+        for i in range(len(Y_1)):
+            if Y_1[i] == Y_2[i] and Y_2[i] == Y_obs[i]:
+                BM[0,0] += 1
+            elif Y_1[i] == Y_2[i] and Y_2[i] != Y_obs[i]:
+                BM[1,1] += 1
+            elif Y_1[i] != Y_2[i] and Y_1[i] == Y_obs[i]:
+                BM[0,1] += 1
+            else :
+                BM[1,0] += 1
+
+    except ValueError:
+        print('Error: The length of the arrays are not equal')
+    return BM
+
 
 
 
@@ -6,24 +38,73 @@
 #==================================Pairwise Diversity==================================
 
 
+#Correlation Coefficient
+def Correlation_Coefficient(Y_1, Y_2, Y_obs):
+    try :
+        len_check(Y_1, Y_2, Y_obs) == True
+        BM = Binary_Matix(Y_1, Y_2, Y_obs)
 
-def Correlation_Diversity(Y_1, Y_2, Y_obs):
-    try:
-        len(Y_1) == len(Y_2) and len(Y_2)  == len(Y_obs)
-        a ,b ,c, d = 0,
-        for i in len(Y_1):
-            if Y_1[i] == Y_2[i] and Y_2[i] == Y_obs[i]:
-                a += 1
-            elif Y_1[i] == Y_2[i] and Y_2[i] != Y_obs[i]:
-                d += 1
-            elif Y_1[i] != Y_2[i] and Y_1[i] == Y_obs[i]:
-                b += 1
-            else :
-                c += 1
-        
-        
-            
+        CC = (BM[0,0]*BM[1,1] - BM[0,1]*BM[1,0])/np.sqrt((BM[0,0] + BM[0,1])*(BM[1,0] + BM[1,1])*(BM[0,0] + BM[1,0])*(BM[0,1] + BM[1,1]))
+
     except ZeroDivisionError :
         print('Error: Division by zero')
-    except ValueError:
-        print('Error:  The length of the arrays are not equal')
+  
+    return CC
+
+#Q-statistic
+
+def Q_statistic(Y_1, Y_2, Y_obs):
+    try:
+        len_check(Y_1, Y_2, Y_obs) == True
+        BM = Binary_Matix(Y_1, Y_2, Y_obs)
+
+        Q = (BM[0,0]*BM[1,1] - BM[0,1]*BM[1,0])/(BM[0,0]*BM[1,1] + BM[0,1]*BM[1,0])
+        
+    except ZeroDivisionError :
+        print('Error: Division by zero')
+    return Q
+
+
+#Differences Mesure
+
+def Differences_Mesure(Y_1, Y_2, Y_obs):
+    try:
+        len_check(Y_1, Y_2, Y_obs) == True
+        BM = Binary_Matix(Y_1, Y_2, Y_obs)
+
+        DM = (BM[0,1] + BM[1,0])/(BM[0,0] + BM[0,1] + BM[1,0] + BM[1,1])
+
+    except ZeroDivisionError :
+        print('Error: Division by zero')
+    return
+
+#Double Fault Measure
+
+def Double_Fault_Measure(Y_1, Y_2, Y_obs):
+    try:
+        len_check(Y_1, Y_2, Y_obs) == True
+        BM = Binary_Matix(Y_1, Y_2, Y_obs)
+
+        DFM = BM[1,1]/(BM[0,0] + BM[0,1] + BM[1,0] + BM[1,1])
+
+    except ZeroDivisionError :
+        print('Error: Division by zero')
+    return DFM
+
+
+#Combination of Differences Mesure and Double Fault Measure
+
+
+def D_DF_Mesure(Y_1, Y_2, Y_obs):
+    try:
+        len_check(Y_1, Y_2, Y_obs) == True
+        BM = Binary_Matix(Y_1, Y_2, Y_obs)
+
+        D_DF = (BM[0,1] + BM[1,0] )/(BM[1,1] )
+
+    except ZeroDivisionError :
+        print('Error: Division by zero')
+    return D_DF
+
+#==================================Non-Pairwise Diversity==================================
+
